a082d075dd09c6c0c1a0b50739f5363aa556124f
TheRhettThompson <rthompsoncareer@gmail.com>
2022-11-15 00:25:42 +0000

    worked on footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,9 +2,17 @@ import React from "react";
 const Footer=()=>{
 return(
 		
-	
+<div className='sticky-lg-bottom bg-dark'>
+    <div className= 'footer-body'>
+        //<h5>Copyright @ Loretta Mayumi Thompson 2022</h5>
+        <p className="footer-text">Copyright @ Loretta Mayumi Thompson 2022</p>
+    </div>
 
-<p>This is the Footer</p>
+</div>	
+    
+
+ 
+
 
 );
 
